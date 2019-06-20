cbe8b7e32f3df46613051e2df5692273faabb3a2
danieltalhaug <daniel.talhaug@outlook.com>
2019-06-20 23:02:03 +0200

    Started work on card details. Pokemon details remains

---
+++ src/components/PokemonDetails/Details.vue
@@ -0,0 +1,96 @@
+<template>
+    <div class="container">
+        <article v-if="$route.params.supertype === 'Pokémon'" class="column">
+            <h1>Pokemon details</h1>
+        </article> <!-- Pokemon card end -->
+        <article v-else-if="$route.params.supertype === 'Trainer'" class="column card">
+            <table class="table is-striped is-hoverable is-fullwidth">
+                <thead class="is-size-4">
+                    <th>Card</th>
+                    <th>{{ name }}</th>
+                </thead>
+                <tbody class="is-size-5">
+                    <tr>
+                        <th>Supertype</th>
+                        <td>{{ supertype }}</td>
+                    </tr>
+                    <tr>
+                        <th>Number</th>
+                        <td>{{ number }}</td>
+                    </tr>
+                    <tr>
+                        <th>Rarity</th>
+                        <td>{{ rarity }}</td>
+                    </tr>
+                    <tr>
+                        <th>Series</th>
+                        <td>{{ series }}</td>
+                    </tr>
+                    <tr>
+                        <th>Artist</th>
+                        <td>{{ artist }}</td>
+                    </tr>
+                    <tr>
+                        <th>Info</th>
+                        <td>{{ text[0] }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </article> <!-- Trainer card end -->
+        <article v-else class="column card">
+            <table class="table is-striped is-hoverable is-fullwidth">
+                <thead class="is-size-4">
+                    <th>Card</th>
+                    <th>{{ name }}</th>
+                </thead>
+                <tbody class="is-size-5">
+                    <tr>
+                        <th>Supertype</th>
+                        <td>{{ supertype }}</td>
+                    </tr>
+                    <tr>
+                        <th>Subtype</th>
+                        <td>{{ subtype }}</td>
+                    </tr>
+                    <tr>
+                        <th>Number</th>
+                        <td>{{ number }}</td>
+                    </tr>
+                    <tr>
+                        <th>Rarity</th>
+                        <td>{{ rarity }}</td>
+                    </tr>
+                    <tr>
+                        <th>Series</th>
+                        <td>{{ series }}</td>
+                    </tr>
+                    <tr>
+                        <th>Artist</th>
+                        <td>{{ artist }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </article> <!-- Energy card end -->
+    </div>
+</template>
+<script>
+export default {
+    name: 'Details',
+    props: [
+        'name',
+        'type',
+        'supertype',
+        'subtype',
+        'number',
+        'artist',
+        'rarity',
+        'series',
+        'text'
+        ]
+}
+</script>
+<style lang="scss" scoped>
+// Import scss variables
+@import "./../../scss/variables.scss";
+// scss
+</style>
--- src/views/PokemonDetails.vue
+++ src/views/PokemonDetails.vue
@@ -4,30 +4,64 @@
             <Title  :title="$route.params.name"
             />
         </section>
-        <section v-if="$route.params.supertype === 'Pokémon'" class="columns">
-            <PokemonHeader  :type="$route.params.type"
-                            :image="$route.params.image"
-                            :name="$route.params.name"
-            />
+        <section v-if="$route.params.supertype === 'Pokémon'">
+            <div class="columns">
+                <PokemonHeader  :type="$route.params.type"
+                                :image="$route.params.image"
+                                :name="$route.params.name"
+                />
+            </div>
+            <div class="columns">
+                <Details        :type="$route.params.type"
+                                :image="$route.params.image"
+                                :name="$route.params.name"
+                />
+            </div>
         </section>
-        <section v-else-if="$route.params.supertype === 'Trainer'" class="columns">
-            <TrainerHeader  :name="$route.params.name"
-                            :image="$route.params.image"
-            />
+        <section v-else-if="$route.params.supertype === 'Trainer'">
+            <div class="columns">
+                <TrainerHeader  :name="$route.params.name"
+                                :image="$route.params.image"
+                />
+            </div>
+            <div class="columns">
+                <Details        :name="$route.params.name"
+                                :supertype="$route.params.supertype"
+                                :number="$route.params.number"
+                                :artist="$route.params.artist"
+                                :rarity="$route.params.rarity"
+                                :series="$route.params.series"
+                                :text="$route.params.text"
+                
+                />
+            </div>
         </section>
-        <section v-else class="columns">
-            <EnergyHeader   :name="$route.params.name"
-                            :image="$route.params.image"
-            />
+        <section v-else>
+            <div class="columns">
+                <EnergyHeader   :name="$route.params.name"
+                                :image="$route.params.image"
+                />
+            </div>
+            <div class="columns">
+                <Details        :name="$route.params.name"
+                                :supertype="$route.params.supertype"
+                                :subtype="$route.params.subtype"
+                                :number="$route.params.number"
+                                :artist="$route.params.artist"
+                                :rarity="$route.params.rarity"
+                                :series="$route.params.series"
+                />
+            </div>
         </section>
     </main>
 </template>
 <script>
 // Imports
 import Title from './../components/Title.vue'
-import PokemonHeader from './../components/Pokemon/PokemonHeader.vue'
-import TrainerHeader from './../components/Trainer/TrainerHeader.vue'
-import EnergyHeader from './../components/Energy/EnergyHeader.vue'
+import PokemonHeader from './../components/PokemonDetails/PokemonHeader.vue'
+import TrainerHeader from './../components/PokemonDetails/TrainerHeader.vue'
+import EnergyHeader from './../components/PokemonDetails/EnergyHeader.vue'
+import Details from './../components/PokemonDetails/Details.vue'
 
 export default {
     name: 'PokemonDetails',
@@ -35,7 +69,8 @@ export default {
         Title,
         PokemonHeader,
         TrainerHeader,
-        EnergyHeader
+        EnergyHeader,
+        Details
     },
 }
 </script>

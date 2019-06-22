22ae4579b37934c1050a4e918356fe0bc603140c
danieltalhaug <daniel.talhaug@outlook.com>
2019-06-22 13:42:54 +0200

    Added datalist to searchbar component. It renders all the card names. Missing the link to cards

--- src/components/Searchbar.vue
+++ src/components/Searchbar.vue
@@ -3,7 +3,10 @@
         <div class="column is-offset-4 is-one-third">
             <div class="field has-addons">
                 <div class="control searchbar--fullwidth">
-                    <input v-model="searchWord" v-on:keyup="sendSearchWord" class="input is-info is-medium is-rounded" type="text" :placeholder="placeholder">
+                    <input list="pokemonNames" v-model="searchWord" v-on:keyup="sendSearchWord" class="input is-info is-medium is-rounded" type="text" :placeholder="placeholder">
+                    <datalist v-if="searchWord" id="pokemonNames">
+                        <option v-for="(value, index) in pokemonCards" :key="index" :value="value.name"></option>
+                    </datalist>
                 </div>
             </div>
         </div>
@@ -17,19 +20,26 @@ export default {
     ],
     data() {
         return {
-            searchWord: ''
+            searchWord: '',
+            pokemonCards: [],
         }
     },
     methods: {
         sendSearchWord: function() {
             this.$emit('getSearchWord', this.searchWord);
         },
-    }
+    },
+    created: function() {
+    // get the pokemonCards array from vuex and set it to cards array on this page
+        this.pokemonCards = this.$store.state.pokemonCards.cards;
+  },
 }
 </script>
 <style lang="scss" scoped>
-
 .searchbar--fullwidth {
     width: 100% !important;
+}
+[list]::-webkit-calendar-picker-indicator {
+    display: none;
 }
 </style>
